ea462ef49455e7ff093eb4f8a4ed8dd870c50357
codingstreams <akxhdev@outlook.com>
2026-09-18 18:28:17 +0530

    fix toggle to enable/disable cache

--- src/main/java/com/example/et/core/config/CacheManagerConfig.java
+++ src/main/java/com/example/et/core/config/CacheManagerConfig.java
@@ -3,6 +3,7 @@ package com.example.et.core.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -18,6 +19,7 @@ import java.time.Duration;
     havingValue = "true"
 )
 @EnableCaching
+@Configuration
 public class CacheManagerConfig {
   @Bean
   public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
